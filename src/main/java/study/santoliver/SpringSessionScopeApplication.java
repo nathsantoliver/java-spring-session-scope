a679ee6b8789939258276ceac179fa7349ae5141
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 20:55:45 -0300

    plublishing the core of the project

---
+++ src/main/java/study/santoliver/SpringSessionScopeApplication.java
@@ -0,0 +1,13 @@
+package study.santoliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/santoliver/SpringSessionScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.santoliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
