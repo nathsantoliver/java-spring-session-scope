5029a0dbd40ae2e9d3ae2f0cb88591d64d1bc472
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 21:26:08 -0300

    logging out the user based on the logout request parameter

--- src/main/java/study/santoliver/controllers/MainController.java
+++ src/main/java/study/santoliver/controllers/MainController.java
@@ -1,7 +1,9 @@
 package study.santoliver.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import study.santoliver.services.LoggedUserManagementService;
 
 @Controller
@@ -14,13 +16,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String home() {
+    public String home(@RequestParam(required = false) String logout, Model model) {
+        if (logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
+
         String username = loggedUserManagementService.getUsername();
 
         if (username == null) {
             return "redirect:/";
         }
 
+        model.addAttribute("username", username);
         return "main.html";
     }
 
