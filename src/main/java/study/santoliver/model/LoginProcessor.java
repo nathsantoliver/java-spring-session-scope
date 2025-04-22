7964ecfb62b0ab06b16b164ecfde06c8e459ec7c
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-21 21:07:58 -0300

    creating the login processor class

---
+++ src/main/java/study/santoliver/model/LoginProcessor.java
@@ -0,0 +1,41 @@
+package study.santoliver.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private String username;
+    private String password;
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+        if ("natalie".equals(username) && "password".equals(password)) {
+            loginResult = true;
+        }
+
+        return loginResult;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
